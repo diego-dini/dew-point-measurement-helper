4f80afcb1f0560c880d57266ebdba9251c6b6fc4
Diego Francisco Ferraz Dini <di-ferraz@hotmail.com>
2025-07-12 00:22:15 +0000

    chore: altera ícone do app e ajusta configurações em app.config

--- app.config.js
+++ app.config.js
@@ -1,10 +1,16 @@
 export default ({ config }) => {
   const isDev = process.env.EAS_BUILD_PROFILE === "development";
+  // You should update these values as needed for each new release!
+  const version = "0.9.3";
+  const buildNumber = "2";
+  const versionCode = 2;
+
   return {
     ...config,
     name: isDev ? "Dew Point Helper (Dev)" : "Dew Point Helper",
     displayName: isDev ? "Dew Point Helper Dev" : "Dew Point Helper",
     icon: "./assets/icon.png",
+    version,
     extra: {
       eas: {
         projectId: "1cc2a678-1fdd-4696-93b8-9661bd1dbe35",
@@ -12,12 +18,14 @@ export default ({ config }) => {
     },
     ios: {
       bundleIdentifier: "com.ruders.dewpointhelper",
+      buildNumber,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
       },
     },
     android: {
       package: "com.ruders.dewpointhelper",
+      versionCode,
     },
   };
 };
